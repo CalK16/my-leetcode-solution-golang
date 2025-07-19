72801519a408f46aec12fcf6ae8f2878797e2900
Calvin K <70356237+CalK16@users.noreply.github.com>
2025-07-20 00:16:41 +0800

    add 49

---
+++ 49_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func sortStr(s string) string {
+	bytes := []byte(s)
+	slices.Sort(bytes)
+	return string(bytes)
+}
+
+func groupAnagrams(strs []string) [][]string {
+	m := make(map[string][]string)
+	for _, word := range strs {
+		s := sortStr(word)
+		m[s] = append(m[s], word)
+	}
+	ans := make([][]string, 0)
+	for _, val := range m {
+		ans = append(ans, val)
+	}
+	return ans
+}
+
+func TestGroupAnagrams(t *testing.T) {
+	expected := [][]string{[]string{"bat"}, []string{"nat", "tan"}, []string{"ate", "eat", "tea"}}
+	ans := groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
+	if !compare2Dslices(ans, expected) {
+		t.Errorf("TEST 1 FAILED")
+
+	}
+
+	expected = [][]string{[]string{""}}
+	ans = groupAnagrams([]string{""})
+	if !compare2Dslices(ans, expected) {
+		t.Errorf("TEST 2 FAILED")
+	}
+
+}
