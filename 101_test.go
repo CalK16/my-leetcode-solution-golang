e224730e6d2b01a71c36532f6ddba5ee2eda54ab
Calvin K <kongzeyuan@gmail.com>
2025-08-02 10:03:29 +0800

    add 101

---
+++ 101_test.go
@@ -0,0 +1,26 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetricHelper(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	} else if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && isSymmetricHelper(left.Left, right.Right) && isSymmetricHelper(left.Right, right.Left)
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isSymmetricHelper(root.Left, root.Right)
+}
